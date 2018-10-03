aa668b02230083be36a4af05d7f9cbb2b4b2a2e6
Łukasz Cieplak <lukaszcieplak.cieplak@gmail.com>
2018-10-03 16:26:12 +0200

    Moblility behavior panel added into sidebar. Loop added for mobility path.

--- js/mobilityBehavior.js
+++ js/mobilityBehavior.js
@@ -6,6 +6,8 @@ function MobilityBehavior(){
 	
 	self.points = new Array();
 	
+	self.loop = false;
+	
 	self.addPoint = function(posx,posy){
 		self.points.push({x:posx, y:posy});
 	}
@@ -27,6 +29,18 @@ function MobilityBehavior(){
 				
 				self.gui.addChild(line);
 				
+		}
+		
+		if(self.loop){
+			
+			var line = new createjs.Shape();
+			line.graphics.setStrokeStyle(3);
+			line.graphics.beginStroke("red");
+			line.graphics.moveTo(self.points[self.points.length-1].x, self.points[self.points.length-1].y);
+			line.graphics.lineTo(self.points[0].x, self.points[0].y);
+			line.graphics.endStroke(); 
+			
+			self.gui.addChild(line);
 		}
 		
 	}
